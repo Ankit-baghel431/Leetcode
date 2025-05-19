7b594fce58bea4172b613de8bd228da6055958b3
Ankit Baghel <ankitbaghel1087@gmail.com>
2025-05-19 15:14:19 +0530

    Time: 30 ms (27.35%) | Memory: 42 MB (27.01%) - LeetSync

---
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] dp = new int[301][11];
+
+    private int solve(int[] jd, int idx, int n, int d) {
+        if (d == 1) {
+            int maxD = jd[idx];
+            for (int i = idx; i < n; i++) {
+                maxD = Math.max(maxD, jd[i]);
+            }
+            return maxD;
+        }
+        if (dp[idx][d] != -1) {
+            return dp[idx][d];
+        }
+        int maxD = jd[idx];
+        int result = Integer.MAX_VALUE;
+        for (int i = idx; i <= n - d; i++) {
+            maxD = Math.max(maxD, jd[i]);
+            int tempresult = maxD + solve(jd, i + 1, n, d - 1);
+            result = Math.min(result, tempresult);
+        }
+        return dp[idx][d] = result;
+    }
+
+    public int minDifficulty(int[] jd, int d) {
+        int n = jd.length;
+        if (n < d)
+            return -1;
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(jd, 0, n, d);
+    }
+}
