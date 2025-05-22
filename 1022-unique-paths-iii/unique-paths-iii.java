1e98fb2b2828292d51f4dbf91e792e3c91159e8b
Ankit Baghel <ankitbaghel1087@gmail.com>
2025-05-22 19:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (82.11%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.java
+++ 1022-unique-paths-iii/unique-paths-iii.java
@@ -1,11 +1,8 @@
 class Solution {
     int totalPaths = 0;
     int totalSteps = 0;
-
     public int uniquePathsIII(int[][] grid) {
         int startX = 0, startY = 0;
-
-        // Count total walkable cells and find starting point
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] != -1) {
@@ -17,34 +14,23 @@ class Solution {
                 }
             }
         }
-
         dfs(grid, startX, startY, 0);
         return totalPaths;
     }
-
     private void dfs(int[][] grid, int i, int j, int steps) {
-        // Out of bounds or obstacle
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == -1) return;
-
-        // If end point
         if (grid[i][j] == 2) {
             if (steps + 1 == totalSteps) {
                 totalPaths++;
             }
             return;
         }
-
-        // Save current cell and mark as visited
         int temp = grid[i][j];
         grid[i][j] = -1;
-
-        // Explore in 4 directions
         dfs(grid, i + 1, j, steps + 1);
         dfs(grid, i - 1, j, steps + 1);
         dfs(grid, i, j + 1, steps + 1);
         dfs(grid, i, j - 1, steps + 1);
-
-        // Backtrack
         grid[i][j] = temp;
     }
 }
