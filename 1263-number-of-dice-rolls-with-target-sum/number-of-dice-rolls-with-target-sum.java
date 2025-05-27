aa4d40f3595c5bcb9c0191a035e569043dcaec10
Ankit Baghel <ankitbaghel1087@gmail.com>
2025-05-27 10:48:28 +0530

    Time: 24 ms (18.89%) | Memory: 42.6 MB (25.08%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,26 +1,17 @@
 class Solution {
     int mod = 1000000007;
-    int [][] dp;  
     public int numRollsToTarget(int n, int k, int target) {
-        dp = new int[n + 1][target + 1];
-
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        int [][] dp = new int[n+1][target+1];
+        dp[0][0] = 1;
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=target;j++){
+                for(int z = 1;z<=k;z++){
+                    if(j-z >= 0){
+                        dp[i][j] = (dp[i][j] + dp[i-1][j-z])%mod;
+                    }
+                }
+            }
         }
-        return helper(n,k,target);
-    }
-    int helper(int n, int k, int target){
-        if(n==0){
-            return target == 0 ? 1 : 0;
-        }
-        if(target < 0) return 0;
-        if(dp[n][target] != -1){
-            return dp[n][target];
-        } 
-        int ways = 0;
-        for(int i = 1;i<=k;i++){
-            ways = (ways+helper(n-1,k,target-i))%mod;
-        }
-        return dp[n][target] = ways;
+        return dp[n][target];
     }
 }
