87f992d2bd76e903508f29a9a61cdf00a53eb266
Ankit Baghel <ankitbaghel1087@gmail.com>
2025-05-28 10:27:34 +0530

    Time: 50 ms (6.22%) | Memory: 61.7 MB (6.35%) - LeetSync

---
+++ 120-triangle/triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int [][] dp ;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        dp = new int[201][10001];
+        for(int d[]:dp){
+            Arrays.fill(d,-1);
+        }
+        return solve(triangle, 0, 0);
+    }
+    private int solve(List<List<Integer>> triangle, int i, int j){
+        if(i == triangle.size()-1){
+            return triangle.get(i).get(j);
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int down = solve(triangle,i+1,j);
+        int diagonal = solve(triangle,i+1,j+1);
+        return dp[i][j] = triangle.get(i).get(j)+Math.min(down,diagonal);
+    }
+}
