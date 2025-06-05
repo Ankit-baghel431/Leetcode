f3da528c73836bd007d787fbae4860f5e03eb10c
Ankit Baghel <ankitbaghel1087@gmail.com>
2025-06-05 11:27:59 +0530

    Time: 0 ms (100%), Space: 41.7 MB (70.02%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,23 +1,37 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) return false;
-        Boolean dp[][]=new Boolean[s1.length()+1][s2.length()+1];
-        return helper(s1,s2,s3,0,0,0,dp);
-    }
-    public boolean helper(String s1, String s2, String s3, int i, int j, int k, Boolean dp[][]){
-        if(i==s1.length() && j==s2.length() && k==s3.length()){
-            return dp[i][j]=true;
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int n3 = s3.length();
+
+        if ((n1 + n2) != n3) {
+            return false;
         }
-        if(dp[i][j]!=null){
+
+        Boolean[][] dp = new Boolean[n1 + 1][n2 + 1];
+
+        return backtrack(s1, s2, s3, 0, 0, 0, dp);
+    }
+
+    private boolean backtrack(String s1, String s2, String s3, int i, int j, int k, Boolean[][] dp) {
+        if (k == s3.length()) {
+            return i == s1.length() && j == s2.length();
+        }
+
+        if (dp[i][j] != null) {
             return dp[i][j];
         }
-        boolean a=false,b=false;
-        if(i<s1.length() && k<s3.length() && s1.charAt(i)==s3.charAt(k)){
-            a= helper(s1,s2,s3,i+1,j,k+1,dp);
+
+        boolean valid = false;
+
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            valid = valid || backtrack(s1, s2, s3, i + 1, j, k + 1, dp);
         }
-        if(j<s2.length() && k<s3.length() && s2.charAt(j)==s3.charAt(k)){
-            b= helper(s1,s2,s3,i,j+1,k+1,dp);
+
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            valid = valid || backtrack(s1, s2, s3, i, j + 1, k + 1, dp);
         }
-        return dp[i][j]=a||b;
+
+        return dp[i][j] = valid;
     }
 }
