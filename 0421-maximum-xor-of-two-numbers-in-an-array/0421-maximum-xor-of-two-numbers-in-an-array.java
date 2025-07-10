02ffcc6ae119970e471529f0fca6b3114fd1b754
Ankit Baghel <ankitbaghel1087@gmail.com>
2025-07-10 15:16:15 +0530

    Time: 18 ms (99.58%), Space: 68 MB (99.65%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,45 +1,31 @@
 class Solution {
-    static class TrieNode {
-        TrieNode[] children = new TrieNode[2];
-    }
-
-    private void insert(TrieNode root, int num) {
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >>> i) & 1;
-            if (root.children[bit] == null) {
-                root.children[bit] = new TrieNode();
-            }
-            root = root.children[bit];
-        }
-    }
-
-    private int findMaxXOR(TrieNode root, int num) {
-        int maxXor = 0;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >>> i) & 1;
-            int toggled = 1 - bit;
-            if (root.children[toggled] != null) {
-                maxXor |= (1 << i);
-                root = root.children[toggled];
-            } else {
-                root = root.children[bit];
-            }
-        }
-        return maxXor;
-    }
-
     public int findMaximumXOR(int[] nums) {
-        TrieNode root = new TrieNode();
         int max = 0;
+        for(var num: nums)
+            max = Math.max(max, num);
+        if(max == 0) return 0;
+        
+        int mask = 0x40000000;
+        while((mask & max) == 0) mask >>= 1;
 
-        for (int num : nums) {
-            insert(root, num);
+        int ret = 0;
+        int masks = 0;
+        while(mask != 0) {
+            masks |= mask;
+            if(check(nums, masks, ret | mask))
+                ret |= mask;
+            mask >>= 1;
         }
+        return ret;
+    }
 
-        for (int num : nums) {
-            max = Math.max(max, findMaxXOR(root, num));
+    private boolean check(int[] nums, int masks, int ans) {
+        Set<Integer> vis = HashSet.newHashSet(nums.length);
+        for(int num : nums) {
+            num &= masks;
+            if(vis.contains(num ^ ans)) return true;
+            vis.add(num);
         }
-
-        return max;
+        return false;
     }
 }
